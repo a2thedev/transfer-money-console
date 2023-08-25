49107b03fe76f655fe8fa084ece50c81477e0e04
a2thedev <anthonygreen048@gmail.com>
2023-08-24 21:39:42 -0400

    Moved real project file over to correct folder

---
+++ Money Transfer Project-Anthony Green/MoneyTransfer_FSF_CSharp_Starter-main/MoneyTransfer/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Person
+    {
+        public string name;
+        private Wallet wallet;
+
+        public Person(string name, int cash)
+
+        {
+            this.name = name;
+            //TODO 2: Assign the value of the 'name' parameter to the 'name' member variable. One line.
+           wallet=new Wallet(cash); 
+            //TODO 3: Instantiate a new 'Wallet' object, passing the 'cash' parameter into its constructor. Assign this new Wallet to the 'wallet' member variable. One line.
+
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{name} has ${wallet.money}");
+            //TODO 4: Use string interpolation in a ConsoleWriteLine to display the user's name and also the amount of money in their wallet (using dot notation). One line.
+
+        }
+
+        public void AcceptMoney(int amount)
+        {
+           wallet.money += amount;
+            //TODO 7: Use an assignment operator to increase the amount of money in the person's wallet by the value of the 'amount' parameter. One line.
+
+        }
+
+        public bool TransferMoney(Person receiver, int amountToTransfer)
+           
+        {
+
+            if (amountToTransfer < wallet.money)
+            {
+
+                wallet.money -= amountToTransfer;
+
+                receiver.AcceptMoney(amountToTransfer);
+
+
+           
+                Console.WriteLine($"Tony gave Bologna $20");
+            }
+
+          else if (amountToTransfer > wallet.money)
+
+            {
+
+                Console.WriteLine("Not enough money to transfer.");
+            }
+            //TODO 8: Use a conditional to check if the 'amountToTransfer' parameter of this method is less than or equal to the amount of money in the wallet of the giver.
+            //If the parameter is less than the amount in the wallet, subtract that amount from the wallet, then call the receiver's AcceptMoney() method and pass 'amountToTransfer' into it. Also use a Console.WriteLine to describe the transaction that occurred.
+            //If the parameter is greater than the amount in the giver's wallet, do not transfer any money and instead print a message describing why the transfer failed.
+
+
+            return true;
+        }
+
+
+    }
+}
---
+++ Money Transfer Project-Anthony Green/MoneyTransfer_FSF_CSharp_Starter-main/MoneyTransfer/Simulation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Simulation
+    {
+        public Simulation()
+        {
+            
+        }
+
+        public void RunSimulation()
+        {
+            Console.WriteLine("Starting simulation");
+
+            Person tony = new Person("Tony",50);
+            Person bologna = new Person("Bologna",60);
+            //TODO 5: Instantiate two Person objects, with unique names and money amounts. Two lines.
+
+            tony.DisplayInfo();
+           bologna.DisplayInfo();
+            //TODO 6: Call the .DisplayInfo() method on each person object, one at a time. This will show their initial money amount. Two lines.
+
+
+            //TEST THE APPLICATION AFTER TODOS 5 AND 6!!
+
+           tony.TransferMoney(bologna,20);
+            
+            //TODO 9: Call the .TransferMoney() method on one person object (giver), making sure to pass in the second person object (receiver), and the amount being transfered. One line.
+
+            tony.DisplayInfo();
+            bologna.DisplayInfo();
+            //TODO 10: Call the .DisplayInfo() method on each person object again, showing that each person's balance has changed. Two lines.
+
+
+            Console.WriteLine("Simulation complete.");
+        }
+    }
+}
